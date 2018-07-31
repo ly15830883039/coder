45ffb75d7c23778f3a17c524edbf226cdf283bc9
zhaorui <zhaorui@ele-cloud.com>
2018-07-31 14:24:35 +0800

    #ILOLA closed  网易2017校园招聘算法题(找数字问题)

--- algorithms/algorithms-other/src/main/java/rui/coder/algorithms/interview/netEase/FindNum.java
+++ algorithms/algorithms-other/src/main/java/rui/coder/algorithms/interview/netEase/FindNum.java
@@ -1,27 +1,25 @@
 package rui.coder.algorithms.interview.netEase;
 
-/**
- *
- */
-public class FindNum {
+class FindNum {
 
     /**
      * 只有一个数没有重复出现过三次
      * @param size 数组的长度
-     * @param ints 数组的内容
+     * @param numbs 数组的内容
      * @return 唯一出现过一次的数
      */
-    public int findNum(int size,int[] ints){
-        //三个数如何处理能够重复三个自动变为1.
-        //相加，取余？
+    int findNum(int size, int[] numbs){
+       int a=0;
+       int b=0;
 
-        //问题复杂了，那么先考虑两个呢？ 如果是两个的位预算呢？
-
-
-        return 0;
+        for (int anInt : numbs) {
+            b=a&(b^anInt);// 和输入找不同 = b，和a比较，只有a中这个位数已经重复才能变1 。
+            a=b|(a^anInt);// 和输入找不同 = a, 和b比较，如果b为1，则把这个位数赋给a。
+            // a=0 b=0 重复0次,或者3的倍数
+            // a=1 b=0 重复1次
+            // a=1 b=1 重复2次
+            // 因为不存在重复2此的，只有3的倍数。所以。最终结果中，a漂移的位数数即为只重复一次的数据。
+        }
+        return a;
     }
-
-
-
-
 }
--- algorithms/algorithms-other/src/test/java/rui/coder/algorithms/interview/netEase/FindNumTest.java
+++ algorithms/algorithms-other/src/test/java/rui/coder/algorithms/interview/netEase/FindNumTest.java
@@ -6,114 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FindNumTest {
 
-    @Test
-    void two() {
-        int i =7;
-
-
-        System.out.println(Integer.toBinaryString(i));
-        int j;
-
-        // 异或 相同为0，不同为1
-        j=i^i;
-
-        System.out.println(Integer.toBinaryString(j));
-
-        //都为1方能为 1
-        j=i&i;
-
-        System.out.println(Integer.toBinaryString(j));
-
-
-        j = i| i;
-        System.out.println(Integer.toBinaryString(j));
-
-
-        System.out.println(Integer.toBinaryString(0&j));
-
-
-        System.out.println(0^1);
-
-        System.out.println(0|1);
-
-    }
-
+    private FindNum findNum=new FindNum();
 
     @Test
-    void three() {
-
-        //两位表示 即 00 01 10
-
-        //位累计到3自动清零
-        int[] nums;
-
-        nums=new int[]{5,11,5,5};
-
-        int a = 0;
-        int b = 0;
-
-        for (int num : nums) {
-            System.out.println(Integer.toBinaryString(num));
-
-            System.out.println(Integer.toBinaryString(b^num));
-            System.out.println(Integer.toBinaryString(a&(b^num)));
-            b=a&(b^num);
-            System.out.println(Integer.toBinaryString(a^num));
-            System.out.println(Integer.toBinaryString(b|(a^num)));
-            a=b|(a^num);
-            System.out.println("a-->"+Integer.toBinaryString(a));
-            System.out.println("b-->"+Integer.toBinaryString(b));
-
-        }
-        System.out.println(a);
-
-
-        /*
-            1. 第一次 b=0, a=记录1 ;
-            2. 第二次
-                1. 不同 b=记录1 , a=记录2
-                    1. 和第一次同， 即为记录1 ，b= 0, [和记录1相同了] a=记录1^记录2
-                        1. 和第一次相同， 即为 b=记录1^记录2&记录1 ； a= 记录1？？？？
-//                    2. 和第一次不同，即为记录3，b=记录2&记录1^记录3  a=
-
-
-         */
-
-
-        /*
-           s     b^num              b=a&(b^num)            a^num              a=b|(a^num)          4 3 2 1
-          0101   0000^0101=0000     0000&0000=0000         0000^0101=0101     0000|0101=0101       0 1 0 1
-          1011   0000^1011=0100     0101&0100=0001         0101^1011=1110     0001|1110=1111       1 1 1 2
-          0101   0001^0101=0100     1110&0100=0100         1111^0101=1010     0100|1010=1110       1 2 1 3  1 2 1 0  1210
-          0101   1011^0101=1110     1110&1110=0000         1110^0101=1011     0000|1011=1011       1 3 1 4  1 0 1 1  1011
-         */
-
-
-        /*
-            b^num
-         */
-
-    }
-
-
-    @Test
-    void name() {
-        System.out.println(110^100);
-
-        System.out.println(Integer.toBinaryString(233));
-        System.out.println(Integer.toBinaryString(123));
-        System.out.println(Integer.toBinaryString(233&123^123));
-        System.out.println(Integer.toBinaryString(233&(123^123)));
-//        System.out.println(Integer.toBinaryString(233^123));
-//        System.out.println(Integer.toBinaryString(123&(233^123)));
-//        System.out.println(Integer.toBinaryString(123&233^123));
-//        System.out.println(Integer.toBinaryString(123^(233^123)));
-//        System.out.println(Integer.toBinaryString(123|(123&(233^123))));
-//
-//
-//        System.out.println(123&(233^123));
-//        System.out.println(233^123);
-
-
+    void findNum() {
+        int[] numbs=new int[]{1,1,7,1};
+        assertSame(7,findNum.findNum(numbs.length,numbs));
     }
 }
