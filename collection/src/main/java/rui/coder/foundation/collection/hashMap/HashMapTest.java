9901060dea1cce105fb6d1b603d965096d8e733d
zhaorui <zhaorui@ele-cloud.com>
2018-10-30 11:34:41 +0800

    fixed #370 两个线程并发put出现问题。

--- collection/src/main/java/rui/coder/foundation/collection/hashMap/HashMapTest.java
+++ collection/src/main/java/rui/coder/foundation/collection/hashMap/HashMapTest.java
@@ -2,15 +2,21 @@ package rui.coder.foundation.collection.hashMap;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class HashMapTest {
 
     private HashMap<MockKey, String> hashMap = new HashMap<>();
 
     @Test
+    @DisplayName("查看put的逻辑，往一个桶中放入，然后出现resize的操作")
     void case1() {
         for (int i = 0; i <= 1000; i++) {
             put(i);
@@ -33,4 +39,31 @@ class HashMapTest {
     private static class MockKey {
         private int key;
     }
+
+
+    @Test
+    @DisplayName("并发put，查看是否线程安全")
+    void currentPut() throws InterruptedException {
+        AtomicInteger atomicInteger=new AtomicInteger(0);
+
+        CountDownLatch countDownLatch=new CountDownLatch(2);
+
+        new Thread(() -> {
+            while (atomicInteger.get()<=1000){
+                put(atomicInteger.getAndIncrement());
+            }
+            countDownLatch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            while (atomicInteger.get()<=1000){
+                put(atomicInteger.getAndIncrement());
+            }
+            countDownLatch.countDown();
+        }).start();
+        countDownLatch.await();
+
+        assertFalse(hashMap.size()==1000);
+    }
+
 }
